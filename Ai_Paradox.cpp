bd1af01c937906bbd5cebe3f724c71f0962b4ffa
Hans <95545028+Hans-xwh@users.noreply.github.com>
2025-09-23 20:39:13 -0500

    Add project files.

---
+++ Ai_Paradox.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+using namespace System;
+
+int main() {
+	system("cls");
+	cout << "Sexoooo\n";
+
+	system("pause");
+	return 0;
+}
